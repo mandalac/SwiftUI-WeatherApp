b31e47dcab8f29f494a1c4a15b354a6035615294
Vasili Mandalac <mandalacvasili1991@gmail.com>
2021-02-09 10:49:44 -0600

    Initial Commit

---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Vasili Mandalac on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
