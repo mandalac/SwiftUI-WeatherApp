edbd1ee9f067672bc6a007f45e51897545561871
Vasili Mandalac <mandalacvasili1991@gmail.com>
2021-02-09 20:47:23 -0600

    Change background color and temperature set

--- SwiftUI-WeatherApp/ContentView.swift
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -8,14 +8,128 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            //Here we adjust LinearGradient for background color
+            BackgroundView(isNight: $isNight)
+            
+            VStack {
+                CityTextView(cityName: "Chicago, IL")
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill",temperature: 76)
+                
+                HStack(spacing: 15) {
+                    //Here are parameters from the extract
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow", temperature: 55)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "snow", temperature: 52)
+                    WeatherDayView(dayOfWeek: "SUN", imageName: "tornado", temperature: 58)
+                }
+                
+                Spacer()
+                
+                Button {
+                    //Will check if is true or fals and display btw dark and light mode
+                    isNight.toggle()
+                    //Label is what the button looks like
+                } label: {
+                    WeatherButton(title: "Change Day Time",
+                                  textColor: .blue,
+                                  backgroundColor: .white)
+                }
+                Spacer()
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+//Extract Subview for refactoring the long code for days of week
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .bold))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+//Extract Subview for refactoring the long code for background color
+struct BackgroundView: View {
+    
+    @Binding var isNight : Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .orange , isNight ? .gray : Color("lightBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            //this will ignore safe area and fill the screen
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+
+struct CityTextView : View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            //Here we add modifiers for the city text
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: imageName)
+                //This gives us multicolor to our SF Symbol
+                .renderingMode(.original)
+                //Make it bigger/resizeable
+                .resizable()
+                //Fits our size 180x180
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        //This is padding between Temperature and days of the week
+        .padding(.bottom, 40)
+    }
+}
+
